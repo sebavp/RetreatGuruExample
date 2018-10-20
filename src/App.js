04e27882f614a0b8085d3b49c2c7564c39548c8f
Sebas <sebastiandfg@gmail.com>
2018-10-20 20:06:45 -0300

    Calendar showing occupancy for Room 5

--- src/App.js
+++ src/App.js
@@ -1,28 +1,172 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import dateFns from 'date-fns';
+import axios from 'axios';
+import moment from 'moment';
+
 import './App.css';
 
-class App extends Component {
-  render() {
+
+const selectedRoom = 'Room 5';
+
+class Calendar extends React.Component {
+  state = {
+    currentMonth: new Date('08/01/2025'),
+    selectedDate: new Date(),
+    registrations: [],
+    occupancy: {}
+  };
+
+  componentDidMount = async () => {
+    const { REACT_APP_API_URL, REACT_APP_API_TOKEN } = process.env;
+    const registrations = await axios.get(`${REACT_APP_API_URL}/registrations?token=${REACT_APP_API_TOKEN}`);
+    this.setState({ registrations: registrations.data });
+  }
+
+  componentDidUpdate = (_, prevState) => {
+    const { registrations, currentMonth } = this.state;
+    if( registrations !== prevState.registrations || currentMonth !== prevState.currentMonth){
+      this.occupancy();
+    }
+  }
+
+  occupancyByDay = day => {
+    const occupancy = this.state.registrations.find(
+      registration =>
+        day.isSameOrAfter(registration.start_date) &&
+        day.isBefore(registration.end_date)
+
+    );
+
+    return occupancy || false;
+  }
+
+  occupancy = () => {
+    const { currentMonth } = this.state;
+    let occupancy = {};
+    for (let day = moment(dateFns.startOfMonth(currentMonth)); day.isSameOrBefore(dateFns.endOfMonth(currentMonth)); day = day.add(1, 'days')) {
+      occupancy[day.date()] = this.occupancyByDay(day);
+    }
+    this.setState({ occupancy });
+  }
+
+  renderHeader() {
+    const dateFormat = 'MMMM YYYY';
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className='header row flex-middle'>
+        <div className='col col-start'>
+          <div className='icon' onClick={this.prevMonth}>
+            chevron_left
+          </div>
+        </div>
+        <div className='col col-center'>
+          <span>{dateFns.format(this.state.currentMonth, dateFormat)}</span>
+        </div>
+        <div className='col col-end' onClick={this.nextMonth}>
+          <div className='icon'>chevron_right</div>
+        </div>
+      </div>
+    );
+  }
+
+  renderDays() {
+    const dateFormat = 'dddd';
+    const days = [];
+
+    let startDate = dateFns.startOfWeek(this.state.currentMonth);
+
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className='col col-center' key={i}>
+          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    }
+
+    return <div className='days row'>{days}</div>;
+  }
+
+  renderCells() {
+    const { currentMonth, selectedDate, occupancy } = this.state;
+    const monthStart = dateFns.startOfMonth(currentMonth);
+    const monthEnd = dateFns.endOfMonth(monthStart);
+    const startDate = dateFns.startOfWeek(monthStart);
+    const endDate = dateFns.endOfWeek(monthEnd);
+
+    const dateFormat = 'D';
+    const rows = [];
+
+    let days = [];
+    let day = startDate;
+    let formattedDate = '';
+
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = dateFns.format(day, dateFormat);
+        const cloneDay = day;
+        const occupancyDay = occupancy[moment(day).date()];
+        const occupied = occupancyDay && occupancyDay.room === selectedRoom && (!this.state.onlyPending || occupancyDay.status === 'pending');
+        days.push(
+          <div
+            className={`col cell ${
+              !dateFns.isSameMonth(day, monthStart)
+                ? 'disabled'
+                : dateFns.isSameDay(day, selectedDate) ? 'selected' : ''
+            } ${ occupied ? 'occupied' : ''}`}
+            title={occupied ? occupancyDay.full_name : null}
+            key={day}
           >
-            Learn React
-          </a>
-        </header>
+            <span className='number'>{formattedDate}</span>
+            <span className='bg'>{formattedDate}</span>
+          </div>
+        );
+        day = dateFns.addDays(day, 1);
+      }
+      rows.push(
+        <div className='row' key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className='body'>{rows}</div>;
+  }
+
+  onDateClick = day => {
+    this.setState({
+      selectedDate: day
+    });
+  };
+
+  nextMonth = () => {
+    this.setState({
+      currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+    });
+  };
+
+  prevMonth = () => {
+    this.setState({
+      currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+    });
+  };
+
+  render() {
+    const available = Object.values(this.state.occupancy).filter(occupied => !(occupied && occupied.room === selectedRoom)).length; 
+    return (
+      <div className='calendar'>
+        <input
+          type='checkbox'
+          value={this.state.onlyPending}
+          onChange={() => this.setState({ onlyPending: !this.state.onlyPending })}
+        />
+        <label>Show only pending</label>
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
+        <p>{available} Available nights</p>
       </div>
     );
   }
 }
 
-export default App;
+export default Calendar;
---
+++ src/testDesriptions.md
@@ -0,0 +1,5 @@
+* if there are no registrations, no days are highlighted, 31 nights availables are shown bellow.
+* if there's one registration starting day 1 and finishing day 6 for "Room 5", days 1 through 5 are highlighted, 26 days availables are shown bellow.
+* if there's one registration for "Room 5" with status "reserved" and the only pending switch is activated, no days are highlighted.
+* if there's one registration starting day 1 and finishing day 6 with status "reserved" for "Room 5" and one starting day 10 and finishing day 15 with status "pending" for "Room 5" and the only pending switch is activated, days 10 through 14 highlighted.
+* if there's one registration starting on day 1 and finishing on day 6 for "Room 6", no days are highlighted.
